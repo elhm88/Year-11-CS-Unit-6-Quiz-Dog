dae71343aa68df29618cbe652bb1640d97e3cb3a
elhm88 <elhm88@student.cis.edu.hk>
2024-02-05 08:20:44 +0800

    First commit.

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,61 @@
 public class Dog {
+    // Properties
+    private int age;
+    private String name;
+    private String color;
+    private Owner owner;
+
+    // Constructor
+    public Dog(int age, String name, String color, Owner owner) {
+        this.age = age;
+        this.name = name;
+        this.color = color;
+        this.owner = owner;
+    }
+
+    // Behaviors
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    private void addOneAge() {
+        age++;
+    }
+
+    public int birthday() {
+        this.addOneAge();
+        return age;
+    }
+
+    public String toString() {
+        return "The dog is named " + name + " and is " + age + " years old. This dog is " + color + ".";
+    }
 }
--- src/Owner.java
+++ src/Owner.java
@@ -1,2 +1,38 @@
 public class Owner {
+    // Properties
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    // Constructor
+    public Owner(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Behaviors
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
